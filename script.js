883779e774f8dc0483163982f29485786d633dfa
nicplackle <nicplackle@gmail.com>
2019-06-19 09:37:21 +0200

    make red block move left, right, up and down within the canvas

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-var myGamePiece = document.getElementById("gamepiece");
+document.getElementById("gamepiece");
 var canvas = document.getElementById("canvas");
 var myGamePieceLeft = 0;
 
 function mov(e) {
-  if (e.keyCode == 39) {
-    myGamePieceLeft += 2;
-    gamepiece.style.left = myGamePieceLeft + "px";
-    if (myGamepieceLeft >= 300) {
-      myGamePieceLeft -= 2;
-    }
-  }
   if (e.keyCode == 37) {
     myGamePieceLeft -= 2;
     gamepiece.style.left = myGamePieceLeft + "px";
@@ -17,10 +10,26 @@ function mov(e) {
       myGamePieceLeft += 2;
     }
   }
-
-  if (e.keyCode == 38) {
+  if (e.keyCode == 39) {
+    myGamePieceLeft += 2;
+    gamepiece.style.left = myGamePieceLeft + "px";
+    if (myGamePieceLeft >= 550) {
+      myGamePieceLeft -= 2;
+    }
   }
   if (e.keyCode == 40) {
+    myGamePieceLeft += 2;
+    gamepiece.style.top = myGamePieceLeft + "px";
+    if (myGamePieceLeft >= 550) {
+      myGamePieceLeft -= 2;
+    }
+  }
+  if (e.keyCode == 38) {
+    myGamePieceLeft -= 2;
+    gamepiece.style.top = myGamePieceLeft + "px";
+    if (myGamePieceLeft <= 0) {
+      myGamePieceLeft += 2;
+    }
   }
 }
 
