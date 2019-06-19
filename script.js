89ce7142301c752ec8b757244d6b253620e86846
nicplackle <nicplackle@gmail.com>
2019-06-19 10:01:37 +0200

    fix skipping block. movement is steady now.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.getElementById("gamepiece");
 var canvas = document.getElementById("canvas");
 var myGamePieceLeft = 0;
+var myGamePieceTop = 0;
 
 function mov(e) {
   if (e.keyCode == 37) {
@@ -18,17 +19,17 @@ function mov(e) {
     }
   }
   if (e.keyCode == 40) {
-    myGamePieceLeft += 2;
-    gamepiece.style.top = myGamePieceLeft + "px";
-    if (myGamePieceLeft >= 550) {
-      myGamePieceLeft -= 2;
+    myGamePieceTop += 2;
+    gamepiece.style.top = myGamePieceTop + "px";
+    if (myGamePieceTop >= 550) {
+      myGamePieceTop -= 2;
     }
   }
   if (e.keyCode == 38) {
-    myGamePieceLeft -= 2;
-    gamepiece.style.top = myGamePieceLeft + "px";
-    if (myGamePieceLeft <= 0) {
-      myGamePieceLeft += 2;
+    myGamePieceTop -= 2;
+    gamepiece.style.top = myGamePieceTop + "px";
+    if (myGamePieceTop <= 0) {
+      myGamePieceTop += 2;
     }
   }
 }
